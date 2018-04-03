debb511ad4e60344c3caf66d9af41bbd3ebb5dac
Brett <brettschultz98@gmail.com>
2018-04-02 21:31:56 -0400

    updated ajax request to get menu from our me WordPress site.  Made a few minor changes to the ajax call and menuBuilder function

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,13 +20,15 @@ function init() {
 
 	$.ajax({
 		method: 'GET',
-		url: 'assets/data/menu.json',
+		url: 'http://me.brettschultzit.com/wp-json/wp-api-menus/v2/menus/3',
 		dataType: 'json',
 		success: function (data) {
 
-			var menu = menuBuilder(data.menu);
+			$('nav').hide();
 
-			$('nav').append(menu);
+			var menu = menuBuilder(data.items);
+
+			$('nav').html(menu).slideDown();
 
 			$("#loaderDiv").fadeOut("slow");
 
@@ -51,11 +53,11 @@ function menuBuilder(obj) {
 
 		obj.forEach(function (item) {
 
-			theMenu = theMenu + '<li><a href="#">' + item.MenuName + '</a>';
+			theMenu = theMenu + '<li><a href="#">' + item.title + '</a>';
 
-			if (item.Menus.length > 0) {
+			if (item.children) {
 
-				theMenu = theMenu + menuBuilder(item.Menus);
+				theMenu = theMenu + menuBuilder(item.children);
 
 			}
 
